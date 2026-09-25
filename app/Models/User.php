f70e16abd5603a6c0d380b8bbd7682c12ae44e54
walidabdalbaki29-ui <walidabdalbaki29@gmail.com>
2026-09-25 11:20:07 +0200

    Add Pint with PSR-12 preset and fix code style

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,8 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
